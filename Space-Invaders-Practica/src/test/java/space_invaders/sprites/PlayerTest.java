aab137351be0b7ac59689d925b713c5130e3dcf7
Angel Rodriguez Moran <angel@MacBook-Pro-de-Angel.local>
2024-10-10 12:37:01 +0200

    first blackbox tests version for sprites.player class

---
+++ Space-Invaders-Practica/src/test/java/space_invaders/sprites/PlayerTest.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import space_invaders.sprites.Player;
+
+class PlayerTest {
+    
+        @Test
+        void testAct() {
+            Player player = new Player();
+            player.act();
+            assertEquals(2, player.getX());
+        }
+    
+        @Test
+        void testKeyPressed() {
+            Player player = new Player();
+            player.keyPressed(KeyEvent.VK_LEFT);
+            assertEquals(-1, player.getDx());
+        }
+
+        @Test
+        void testKeyReleased() {
+            Player player = new Player();
+            player.keyReleased(KeyEvent.VK_LEFT);
+            assertEquals(0, player.getDx());
+        }
+
+        @Test 
+        void testInitPlayer() {
+            Player player = new Player();
+            assertEquals(270, player.getX());
+            assertEquals(280, player.getY());
+        }
+
+}
+
