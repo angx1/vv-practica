51033d52988d370b35ae6c2f0da916f5bc566aaa
Miguel Ángel Sicilia GarcÍa <m.sicilia.garcia@alumnos.upm.es>
2024-11-07 12:42:46 +0100

    Comments

--- Space-Invaders-Practica/src/test/java/space_invaders/sprites/SpriteTest.java
+++ Space-Invaders-Practica/src/test/java/space_invaders/sprites/SpriteTest.java
@@ -5,12 +5,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SpriteTest {
 
+    //Se ignoran las pruebas de los getters y setters ya que son generados automaticamente
+
     @Test
     void testDie() {
         Sprite sprite = new Sprite();
         sprite.die();
         assertFalse(sprite.isVisible());
     }
+
+    //Se prueban isVisible y isDying ya que como no pone get se considera que se han realizado a mano
 
     @Test
     void testIsVisible() {
