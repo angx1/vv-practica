51db2f74c546edff57a9d4f742365aeee2822680
Miguel Ángel Sicilia GarcÍa <m.sicilia.garcia@alumnos.upm.es>
2024-11-07 12:21:35 +0100

    Pararmetrized Test for Alien and Updated Dependencies

--- Space-Invaders-Practica/src/test/java/space_invaders/sprites/AlienTest.java
+++ Space-Invaders-Practica/src/test/java/space_invaders/sprites/AlienTest.java
@@ -1,176 +1,77 @@
 package space_invaders.sprites;
 
-import main.Commons;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AlienTest {
 
-    //TODO Método initAlien
-    @Test
-    void initAlien_xMinMinus() {
-        Alien alien = new Alien(-1, 175);
-        assertEquals(0, alien.getX());
+    @ParameterizedTest
+    @CsvSource({
+        "-1, 175, 0",    // xMinMinus
+        "0, 175, 0",     // xMin
+        "1, 175, 1",     // xMinPlus
+        "357, 175, 357", // xMaxMinus
+        "358, 175, 358", // xMax
+        "359, 175, 358"  // xMaxPlus
+    })
+    void testInitAlienX(int inputX, int inputY, int expectedX) {
+        Alien alien = new Alien(inputX, inputY);
+        assertEquals(expectedX, alien.getX());
     }
 
-    @Test
-    void initAlien_xMin() {
-        Alien alien = new Alien(0, 175);
-        assertEquals(0, alien.getX());
+    @ParameterizedTest
+    @CsvSource({
+        "179, -1, 0",    // yMinMinus
+        "179, 0, 0",     // yMin
+        "179, 1, 1",     // yMinPlus
+        "179, 349, 349", // yMaxMinus
+        "179, 350, 350", // yMax
+        "179, 351, 350"  // yMaxPlus
+    })
+    void testInitAlienY(int inputX, int inputY, int expectedY) {
+        Alien alien = new Alien(inputX, inputY);
+        assertEquals(expectedY, alien.getY());
     }
 
-    @Test
-    void initAlien_xMinPlus() {
-        Alien alien = new Alien(1, 175);
-        assertEquals(1, alien.getX());
+    @ParameterizedTest
+    @CsvSource({
+        "-1, 175, 0",    // xMinMinus
+        "0, 175, 0",     // xMin
+        "1, 175, 1",     // xMinPlus
+        "357, 175, 357", // xMaxMinus
+        "358, 175, 358", // xMax
+        "359, 175, 358"  // xMaxPlus
+    })
+    void testInitBombX(int inputX, int inputY, int expectedX) {
+        Alien alien = new Alien(inputX, inputY);
+        assertEquals(expectedX, alien.getBomb().getX());
     }
 
-    @Test
-    void initAlien_xMaxMinus() {
-        Alien alien = new Alien(357, 175);
-        assertEquals(357, alien.getX());
+    @ParameterizedTest
+    @CsvSource({
+        "179, -1, 0",    // yMinMinus
+        "179, 0, 0",     // yMin
+        "179, 1, 1",     // yMinPlus
+        "179, 349, 349", // yMaxMinus
+        "179, 350, 350", // yMax
+        "179, 351, 350"  // yMaxPlus
+    })
+    void testInitBombY(int inputX, int inputY, int expectedY) {
+        Alien alien = new Alien(inputX, inputY);
+        assertEquals(expectedY, alien.getBomb().getY());
     }
 
-    @Test
-    void initAlien_xMax() {
-        Alien alien = new Alien(358, 175);
-        assertEquals(Commons.BOARD_WIDTH, alien.getX());
-    }
-
-    @Test
-    void initAlien_xMaxPlus() {
-        Alien alien = new Alien(359, 175);
-        assertEquals(Commons.BOARD_WIDTH, alien.getX());
-    }
-
-    @Test
-    void initAlien_yMinMinus() {
-        Alien alien = new Alien(179, -1);
-        assertEquals(0, alien.getY());
-    }
-
-    @Test
-    void initAlien_yMin() {
-        Alien alien = new Alien(179, 0);
-        assertEquals(0, alien.getY());
-    }
-
-    @Test
-    void initAlien_yMinPlus() {
-        Alien alien = new Alien(179, 1);
-        assertEquals(1, alien.getY());
-    }
-
-    @Test
-    void initAlien_yMaxMinus() {
-        Alien alien = new Alien(179, 349);
-        assertEquals(349, alien.getY());
-    }
-
-    @Test
-    void initAlien_yMax() {
-        Alien alien = new Alien(179, 350);
-        assertEquals(Commons.BOARD_HEIGHT, alien.getY());
-    }
-
-    @Test
-    void initAlien_yMaxPlus() {
-        Alien alien = new Alien(179, 351);
-        assertEquals(Commons.BOARD_HEIGHT, alien.getY());
-    }
-
-    @Test
-    void initAlien_nominal() {
-        Alien alien = new Alien(179, 175);
-        assertEquals(179, alien.getX());
-        assertEquals(175, alien.getY());
-    }
-
-    //TODO Método initBomb
-    @Test
-    void initBomb_xMinMinus() {
-        Alien alien = new Alien(-1, 175);
-        assertEquals(0, alien.getBomb().getX());
-    }
-
-    @Test
-    void initBomb_xMin() {
-        Alien alien = new Alien(0, 175);
-        assertEquals(0, alien.getBomb().getX());
-    }
-
-    @Test
-    void initBomb_xMinPlus() {
-        Alien alien = new Alien(1, 175);
-        assertEquals(1, alien.getBomb().getX());
-    }
-
-    @Test
-    void initBomb_xMaxMinus() {
-        Alien alien = new Alien(357, 175);
-        assertEquals(357, alien.getBomb().getX());
-    }
-
-    @Test
-    void initBomb_xMax() {
-        Alien alien = new Alien(358, 175);
-        assertEquals(Commons.BOARD_WIDTH, alien.getBomb().getX());
-    }
-
-    @Test
-    void initBomb_xMaxPlus() {
-        Alien alien = new Alien(359, 175);
-        assertEquals(Commons.BOARD_WIDTH, alien.getBomb().getX());
-        assertEquals(Commons.BOARD_HEIGHT, alien.getBomb().getY());
-    }
-
-    @Test
-    void initBomb_yMinMinus() {
-        Alien alien = new Alien(179, -1);
-        assertEquals(0, alien.getBomb().getY());
-    }
-
-    @Test
-    void initBomb_yMin() {
-        Alien alien = new Alien(179, 0);
-        assertEquals(0, alien.getBomb().getY());
-    }
-
-    @Test
-    void initBomb_yMinPlus() {
-        Alien alien = new Alien(179, 1);
-        assertEquals(1, alien.getBomb().getY());
-    }
-
-    @Test
-    void initBomb_yMaxMinus() {
-        Alien alien = new Alien(179, 349);
-        assertEquals(349, alien.getBomb().getY());
-    }
-
-    @Test
-    void initBomb_yMax() {
-        Alien alien = new Alien(179, 350);
-        assertEquals(Commons.BOARD_HEIGHT, alien.getBomb().getY());
-    }
-
-    @Test
-    void initBomb_yMaxPlus() {
-        Alien alien = new Alien(179, 351);
-        assertEquals(Commons.BOARD_HEIGHT, alien.getBomb().getY());
-        assertEquals(Commons.BOARD_WIDTH, alien.getBomb().getX());
-    }
-
-    @Test
-    void initBomb_nominal() {
-        Alien alien = new Alien(179, 175);
-        assertEquals(179, alien.getBomb().getX());
-        assertEquals(175, alien.getBomb().getY());
-    }
-
-    @Test
-    void testIsBombDestroyed() {
-        Alien alien = new Alien(179, 175);
+    @ParameterizedTest
+    @CsvSource({
+        "179, 175, 179, 175"  // nominal case
+    })
+    void testInitAlienNominal(int inputX, int inputY, int expectedX, int expectedY) {
+        Alien alien = new Alien(inputX, inputY);
+        assertEquals(expectedX, alien.getX());
+        assertEquals(expectedY, alien.getY());
+        assertEquals(expectedX, alien.getBomb().getX());
+        assertEquals(expectedY, alien.getBomb().getY());
         assertTrue(alien.getBomb().isDestroyed());
     }
 }
