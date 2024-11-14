c8ed1a5a6cba3915298af5043d3ef446ccb560de
AVALON1492 <alexlozanomartin1492@gmail.com>
2024-11-14 12:54:57 +0100

    Creación de la nueva carpera para la caja blanca

---
+++ SpaceInvaders_Nivel2/src/test/java/space_invaders/sprites/AlienTest.java
@@ -0,0 +1,11 @@
+package space_invaders.sprites;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlienTest {
+
+
+
+}
