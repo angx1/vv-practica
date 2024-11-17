a829d00d0649b12dfda985c7d0d70b6f03f631d8
AVALON1492 <alexlozanomartin1492@gmail.com>
2024-11-17 11:22:57 +0100

    Revisión final de la caja Blanca de AlienTest

--- SpaceInvaders_Nivel2/src/test/java/space_invaders/sprites/AlienTest.java
+++ SpaceInvaders_Nivel2/src/test/java/space_invaders/sprites/AlienTest.java
@@ -1,16 +1,33 @@
 package space_invaders.sprites;
 
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AlienTest {
 
-    @Test
-    void initAlien1() {
-        Alien alien = new Alien(179, -1);
-        assertEquals(179, alien.getX());
-        assertEquals(0, alien.getY());
+    @ParameterizedTest
+    @CsvSource({
+            "179, -1, 179, 0",      //1
+            "179, 175, 179, 175",   //2
+            "-1, -1, 0, 0",         //4
+    })
+    void testInitAlien(int inputX, int inputY, int expectedX, int expectedY) {
+        Alien alien = new Alien(inputX, inputY);
+        assertEquals(expectedX, alien.getX());
+        assertEquals(expectedY, alien.getY());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "359, 175, 358, 175",   //1
+            "179, 351, 179, 350",   //2
+            "179, 175, 179, 175",   //4
+    })
+    void testInitBomb(int inputX, int inputY, int expectedX, int expectedY) {
+        Alien alien = new Alien(inputX, inputY);
+        assertEquals(expectedX, alien.getBomb().getX());
+        assertEquals(expectedY, alien.getBomb().getY());
     }
 
 }
